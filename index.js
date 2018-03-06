a9970f39da6d9b5b22085b76e1b01e72fbb5b4d3
Raj Trivedi <rise.raj@outlook.com>
2018-03-06 15:51:06 +0530

    First commit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const host = 'https://query.yahooapis.com';
 const server = express();
 server.use(bodyParser.json());
 
-server.post('/get-movie-details', function (req, res) {
+server.post('/get-details', function (req, res) {
 //exports.get_movie_details = (req, res) => {
   //response = "This is Raj's Custom Msg!" + req.body.message //Default response from the webhook to show it's working
   //res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
@@ -60,18 +60,26 @@ server.post('/get-movie-details', function (req, res) {
         //     res.send(JSON.stringify({ 'speech': output, 'displayText': output }));
         // });
         //let path = encodeURI(host + '/v1/public/yql?q=select%20wind%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text="' + encodeURIComponent(cityName)+'"&format=json');
-        let path = host + '/v1/public/yql?q=select item.condition from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + cityName +'")&format=json';
+        let path = host + '/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + cityName +'") and u="c"&format=json';
         console.log(path);
         http.get(path, (responseFromAPI) => {
 
             responseFromAPI.on('data', function (chunk) {
-                let weather_info = JSON.parse(chunk)['query']['results']['channel']['item']['condition'];
+                let weather_info = JSON.parse(chunk)['query'];
+                //let weather_info = JSON.parse(chunk)['query']['results']['channel']['item']['condition'];
+                console.log(weather_info.results.channel.item.condition.temp);
+                //console.log(weather_info.results.channel.item.forecast[0].text);
                 if(weather_info === undefined){
                     console.log("weather_info is empty");
                 }else{
                     //console.log(weather_info);
                     //console.log(weather_info.temp);
-                    let dataToSend = 'Weather in ' + cityName + ' is ' + Number(((weather_info.temp - 32) * (5/9))).toFixed(0) + '.';
+                    let dataToSend = 'Today the temperature in ' + cityName + ' is ' + weather_info.results.channel.item.condition.temp + '°C, it will be ' + weather_info.results.channel.item.condition.text +' ';
+                    dataToSend += '\nForecast for next 7 days:'
+                    for (var i = 1; i <= 7; i++){
+                        dataToSend += '\n' + weather_info.results.channel.item.forecast[i].day + ' - ' + weather_info.results.channel.item.forecast[i].text + '. High: ' + weather_info.results.channel.item.forecast[i].high + '°C' + ' Low: ' + weather_info.results.channel.item.forecast[i].low + '°C';
+                    }
+                    console.log(dataToSend);
                     return res.json({
                         speech: dataToSend,
                         displayText: dataToSend,

12c355e1b39cb4674ce42261f8de564ade028458
Raj Trivedi <32588108+irajtrivedi@users.noreply.github.com>
2018-03-12 12:24:11 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -18,51 +18,37 @@ server.post('/get-details', function (req, res) {
             speech: 'Sorry! Please try again',
             displayText: 'Sorry! Please try again'
         });
-    } else if (cityName != undefined){
-        fetch_weather_info(cityName)
+    } else if (cityName != undefined) {
+        let dataToSend = fetch_weather_info(cityName);
+        return res.json({
+            speech: dataToSend,
+            displayText: dataToSend
+        });
     }
 });
 
 function fetch_weather_info(cityName) {
     let path = weather_host + '/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + cityName + '") and u="c"&format=json';
-    console.log(path);
-    try {
-        http.get(path, (responseFromAPI) => {
-            responseFromAPI.on('data', function (chunk) {
-                let weather_info = JSON.parse(chunk)['query'];
-                if (weather_info.count == 0) {
-                    return res.json({
-                        speech: 'Sorry! Weather not found for ' + cityName,
-                        displayText: 'Sorry! Weather not found for ' + cityName,
-                        source: 'get-details'
-                    });
-                } else {
-                    let dataToSend = 'Today the temperature in ' + cityName + ' is ' + weather_info.results.channel.item.condition.temp + '°C, it will be ' + weather_info.results.channel.item.condition.text + '.';
-                    dataToSend += ' Tomorrow it will be ' + weather_info.results.channel.item.forecast[1].text + ',';
-                    dataToSend += ' high: ' + weather_info.results.channel.item.forecast[1].high + '°C';
-                    dataToSend += ' and low: ' + weather_info.results.channel.item.forecast[1].low + '°C.';
-                    console.log(dataToSend);
-                    return res.json({
-                        speech: dataToSend,
-                        displayText: dataToSend,
-                        source: 'get-details'
-                    });
-                }
-            });
-        }, (error) => {
-            return res.json({
-                speech: 'Something went wrong!',
-                displayText: 'Something went wrong!',
-                source: 'get-details'
-            });
+    //console.log(path);
+    let dataToSend = '';
+    var req = http.get(path, (responseFromAPI) => {
+        responseFromAPI.on('data', function (chunk) {
+            let weather_info = JSON.parse(chunk)['query'];
+            if (weather_info.count == 0) {
+                dataToSend = 'Sorry! Weather not found for ' + cityName;
+            } else {
+                dataToSend = 'Today the temperature in ' + cityName + ' is ' + weather_info.results.channel.item.condition.temp + '°C, it will be ' + weather_info.results.channel.item.condition.text + '.';
+                dataToSend += ' Tomorrow it will be ' + weather_info.results.channel.item.forecast[1].text + ',';
+                dataToSend += ' high: ' + weather_info.results.channel.item.forecast[1].high + '°C';
+                dataToSend += ' and low: ' + weather_info.results.channel.item.forecast[1].low + '°C.';
+            }
         });
-    } catch (e) {
-        return res.json({
-            speech: 'Something went wrong!',
-            displayText: 'Something went wrong!',
-            source: 'get-details'
-        });
-    }
+    });
+    req.on('error', function(e){
+        console.log('problem with request: ' + e.message);
+        dataToSend = 'Sorry! Something went wrong';
+    });
+    return dataToSend;
 }
 
 server.listen((process.env.PORT || 8000), function () {
